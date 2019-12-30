57f832399212dad822fd9d2d4ee9238107399792
Canmor Lam <canmor.lam@gmail.com>
2019-12-30 10:10:45 +0800

    Amend to compliant cpplint check

--- demos/hello_world/main.cc
+++ demos/hello_world/main.cc
@@ -1,3 +1,6 @@
+/*
+ * Copyright (c) 2019 ThoughtWorks Inc.
+ */
 #include <cdcf/cdcf.h>
 
 int main(int argc, char *argv[]) { return 0; }
--- node_keeper/src/main.cc
+++ node_keeper/src/main.cc
@@ -1,1 +1,4 @@
+/*
+ * Copyright (c) 2019 ThoughtWorks Inc.
+ */
 int main(int argc, char *argv[]) { return 0; }
