9256779386d200d8900380ae3fc1d92103632f9e
mingfei.deng <mingfei.deng@thoughtworks.com>
2020-01-07 16:17:18 +0800

    format

--- config_manager/src/cdcf_config.cc
+++ config_manager/src/cdcf_config.cc
@@ -1,20 +1,16 @@
 #include "cdcf_config.h"
 
 cdcf_config::RET_VALUE cdcf_config::parse_config(int argc, char** argv,
-                                                 const char* ini_file_cstr){
-    if (auto err = parse(argc, argv, ini_file_cstr)) {
-        std::cerr << "error while parsing CLI and file options: "
-                  << caf::actor_system_config::render(err) << std::endl;
-        return FAILURE;
-    }
+                                                 const char* ini_file_cstr) {
+  if (auto err = parse(argc, argv, ini_file_cstr)) {
+    std::cerr << "error while parsing CLI and file options: "
+              << caf::actor_system_config::render(err) << std::endl;
+    return FAILURE;
+  }
 
-    if (cli_helptext_printed) {
-        return FAILURE;
-    }
+  if (cli_helptext_printed) {
+    return FAILURE;
+  }
 
-    return SUCCESS;
+  return SUCCESS;
 }
-
-
-
-
