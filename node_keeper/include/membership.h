922af85b68525f442b94ad1b39144d6fb960f4e9
davidzwb <davidzwb@gmail.com>
2020-01-15 14:50:49 +0800

    add Config class with host member entry.

--- node_keeper/include/membership.h
+++ node_keeper/include/membership.h
@@ -6,8 +6,12 @@
 #define CDCF_MEMBERSHIP_H
 
 #include <string>
+#include <unordered_map>
+#include <vector>
 
 namespace membership {
+
+// TODO Error Code
 
 class Member {
  private:
@@ -16,13 +20,39 @@ class Member {
   short port_;
 
  public:
+  Member() : node_name_(""), ip_address_(""), port_(0) {}
   Member(const std::string& node_name, const std::string& ip_address,
-         short port_)
-      : node_name_(node_name), ip_address_(ip_address), port_(port_) {}
+         short port)
+      : node_name_(node_name), ip_address_(ip_address), port_(port) {}
+
+  friend bool operator==(const Member& lhs, const Member& rhs);
+  friend bool operator!=(const Member& lhs, const Member& rhs);
 
   const std::string& GetNodeName() const { return node_name_; }
   const std::string& GetIpAddress() const { return ip_address_; }
   short GetPort() const { return port_; }
+};
+
+class Config {
+ public:
+  int AddHostMember(const std::string& node_name, const std::string& ip_address,
+                    short port);
+  Member GetHostMember() { return host_; }
+
+ private:
+  Member host_;
+};
+
+class Membership {
+ public:
+  int Init(const std::string& node_name, const std::string& ip_address,
+           short port);
+  std::vector<Member> GetMembers();
+
+ private:
+  int addMember(const Member& member);
+
+  std::vector<Member> members_;
 };
 
 };  // namespace membership
