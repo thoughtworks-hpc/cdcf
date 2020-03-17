a732b5acbda58750c857a5d2622b1fd8baa42e54
Canmor Lam <canmor.lam@gmail.com>
2020-03-17 10:50:59 +0800

    Remove duplicate of gossip message test

--- node_keeper/src/gossip/message_test.cc
+++ node_keeper/src/gossip/message_test.cc
@@ -5,29 +5,45 @@
 
 #include <gmock/gmock.h>
 
+#include <iterator>
 #include <vector>
 
-using testing::Eq;
+using testing::Eq, testing::ContainerEq;
 
-TEST(GossipMessage, ShouldDecodeMessageRight) {
-  std::vector<uint8_t> buffer{0, 0, 0, 5, gossip::Message::Type::kPush,
-                              1, 2, 3, 4, 5};
+class GossipMessage : public testing::Test {
+ protected:
+  auto Encapsulate(enum gossip::Message::Type type,
+                   const std::vector<uint8_t>& data) {
+    /* FIXME: consider size more than 255 */
+    const auto size_lowest_byte = static_cast<uint8_t>(data.size());
+    auto buffer = std::vector<uint8_t>{0, 0, 0, size_lowest_byte, type};
+    std::copy(data.begin(), data.end(), std::back_inserter(buffer));
+    return buffer;
+  }
+
+  static const std::vector<uint8_t> kData;
+};
+
+const std::vector<uint8_t> GossipMessage::kData{1, 2, 3, 4, 5};
+
+TEST_F(GossipMessage, ShouldDecodeMessageRight) {
+  const auto type = gossip::Message::Type::kPush;
+  const auto buffer = Encapsulate(type, kData);
+
   gossip::Message message;
-
-  auto consumed = message.Decode(buffer.data(), buffer.size());
+  const auto consumed = message.Decode(buffer.data(), buffer.size());
 
   EXPECT_THAT(consumed, Eq(buffer.size()));
-  EXPECT_THAT(message.Type(), Eq(gossip::Message::Type::kPush));
-  EXPECT_THAT(message.Data(), Eq(std::vector<uint8_t>{1, 2, 3, 4, 5}));
+  EXPECT_THAT(message.Type(), Eq(type));
+  EXPECT_THAT(message.Data(), ContainerEq(kData));
 }
 
-TEST(GossipMessage, ShouldEncodeMessageRight) {
-  std::vector<uint8_t> data{1, 2, 3, 4, 5};
-  auto type = gossip::Message::Type::kPull;
-  gossip::Message message(type, data.data(), data.size());
+TEST_F(GossipMessage, ShouldEncodeMessageRight) {
+  const auto type = gossip::Message::Type::kPull;
+  gossip::Message message(type, kData.data(), kData.size());
 
   auto buffer = message.Encode();
 
-  EXPECT_THAT(buffer,
-              Eq(std::vector<uint8_t>{0, 0, 0, 5, type, 1, 2, 3, 4, 5}));
+  const auto expect = Encapsulate(type, kData);
+  EXPECT_THAT(buffer, ContainerEq(expect));
 }
