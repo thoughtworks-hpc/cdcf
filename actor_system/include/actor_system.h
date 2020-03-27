785f1f5e7310002c0d3362cdbf54caf3220b5c13
Canmor Lam <canmor.lam@gmail.com>
2020-03-27 10:06:04 +0800

    Add cluster to actor system

---
+++ actor_system/include/actor_system.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2019-2020 ThoughtWorks Inc.
+ */
+#ifndef ACTOR_SYSTEM_INCLUDE_ACTOR_SYSTEM_H_
+#define ACTOR_SYSTEM_INCLUDE_ACTOR_SYSTEM_H_
+#include <caf/all.hpp>
+#include <caf/io/all.hpp>
+
+#include "actor_system/cluster.h"
+#include "actor_system/config.h"
+#endif  // ACTOR_SYSTEM_INCLUDE_ACTOR_SYSTEM_H_
---
+++ actor_system/include/actor_system/config.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2019-2020 ThoughtWorks Inc.
+ */
+#ifndef ACTOR_SYSTEM_INCLUDE_ACTOR_SYSTEM_CONFIG_H_
+#define ACTOR_SYSTEM_INCLUDE_ACTOR_SYSTEM_CONFIG_H_
+#include <cdcf_config.h>
+
+#include <cstdint>
+#include <string>
+
+namespace actor_system {
+class Config : public cdcf_config {
+ public:
+  std::string host_ = "localhost";
+  uint16_t port_ = 4750;
+
+  Config() {
+    opt_group{custom_options_, "global"}
+        .add(host_, "host,H", "set host")
+        .add(port_, "actor_system_port,a", "set port for actor_system");
+  }
+};
+}  // namespace actor_system
+#endif  // ACTOR_SYSTEM_INCLUDE_ACTOR_SYSTEM_CONFIG_H_
