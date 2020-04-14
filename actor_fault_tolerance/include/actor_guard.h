3619463c33bedcbe2b5be059f4653ec04f9188e2
mingfei.deng <mingfei.deng@thoughtworks.com>
2020-04-14 16:54:46 +0800

    Fix actor guard. when actor return business error, system will not restart actor

--- actor_fault_tolerance/include/actor_guard.h
+++ actor_fault_tolerance/include/actor_guard.h
@@ -16,11 +16,17 @@ class ActorGuard {
         restart_fun_(std::move(restart)) {}
 
   template <class... send, class return_function>
-  bool SendAndReceive(return_function f, const send&... xs) {
+  bool SendAndReceive(return_function f,
+                      std::function<void(caf::error)> err_deal,
+                      const send&... xs) {
     if (active_) {
       caf::message send_message = caf::make_message(xs...);
       message_guarantor_.SendAndReceive(
-          0, f, [&](caf::error) { HandleSendFailed(send_message, f); }, xs...);
+          0, f,
+          [&](caf::error err) {
+            HandleSendFailed(send_message, f, err_deal, err);
+          },
+          xs...);
     }
 
     return active_;
@@ -28,20 +34,27 @@ class ActorGuard {
 
  private:
   template <class return_function>
-  void HandleSendFailed(const caf::message& msg, return_function f) {
-    std::cout << "send  msg failed, try restart dest actor." << std::endl;
+  void HandleSendFailed(const caf::message& msg, return_function f,
+                        std::function<void(caf::error)> err_deal,
+                        caf::error err) {
+    if ("system" != caf::to_string(err.category())) {
+      // not system error, mean actor not down, this is a business error.
+      err_deal(err);
+      return;
+    }
+
+    std::cout << "send msg failed, try restart dest actor." << std::endl;
     message_guarantor_.SetReceiver(restart_fun_(active_));
 
     if (active_) {
       std::cout << "restart actor success." << std::endl;
-      (void)SendAndReceive(f, msg);
+      (void)SendAndReceive(f, err_deal, msg);
     } else {
       std::cout << "restart actor failed. message:" << caf::to_string(msg)
                 << " will not deliver." << std::endl;
     }
   }
 
-  void HandleDownMsg(const std::string& actor_description);
   ActorMessageGuarantor message_guarantor_;
   std::function<caf::actor(std::atomic<bool>&)> restart_fun_;
   std::atomic<bool> active_ = true;
