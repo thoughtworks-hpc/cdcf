027a7f711f9eebdbbb0e5edfceb6988275e80d58
Canmor Lam <canmor.lam@gmail.com>
2020-04-17 14:29:30 +0800

    Add demo for cluster

---
+++ demos/cluster/config.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2019-2020 ThoughtWorks Inc.
+ */
+#ifndef DEMOS_CLUSTER_CONFIG_H_
+#define DEMOS_CLUSTER_CONFIG_H_
+#include <actor_system.h>
+
+class Config : public actor_system::Config {
+ public:
+  uint16_t reception_port_ = 3335;
+
+ public:
+  Config() {
+    opt_group{custom_options_, "global"}.add(
+        reception_port_, "reception_port,r", "port for reception");
+  }
+};
+#endif  // DEMOS_CLUSTER_CONFIG_H_
