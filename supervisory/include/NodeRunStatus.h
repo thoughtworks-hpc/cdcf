7d674847b1d9de6341f02fd0985886907e362f1d
mingfei.deng <mingfei.deng@thoughtworks.com>
2020-05-21 10:04:52 +0800

    add supervisory of node status

---
+++ supervisory/include/NodeRunStatus.h
@@ -0,0 +1,42 @@
+//
+// Created by Mingfei Deng on 2020/5/15.
+//
+
+#ifndef UNTITLED_NODERUNSTATUS_H
+#define UNTITLED_NODERUNSTATUS_H
+#include <cstdio>
+#include <memory>
+#include <mutex>
+
+struct MemoryStatus {
+    unsigned long max_memory;
+    unsigned long use_memory;
+    double useRate;
+};
+
+class NodeRunStatus {
+public:
+    static NodeRunStatus* GetInstance();
+    double GetCpuRate();
+    int GetMemoryState(MemoryStatus &memory_info);
+
+private:
+    struct CpuInfo{
+        unsigned long user;
+        unsigned long nice;
+        unsigned long system;
+        unsigned long idle;
+        unsigned long io_wait;
+        unsigned long irq;
+        unsigned long soft_irq;
+    };
+
+    NodeRunStatus(FILE *memoryFile, FILE *cpuFile);
+    void GetCpuInfo(CpuInfo& cpu_info);
+
+    FILE *memory_file;
+    FILE *cpu_file;
+};
+
+
+#endif//UNTITLED_NODERUNSTATUS_H
