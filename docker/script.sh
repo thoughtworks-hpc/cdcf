c266bacce746a1c2cbe6df8cbb2633ecc07e6ad7
mingfei.deng <mingfei.deng@thoughtworks.com>
2020-05-25 17:26:07 +0800

    Add set threads_proportion

--- docker/script.sh
+++ docker/script.sh
@@ -3,5 +3,5 @@ echo "running on host: $HOST"
 echo "seeds: $SEEDS"
 echo /bin/node_keeper --name=$HOST --seeds=$SEEDS --host=$HOST --port=4445
 /bin/node_keeper --name=$HOST --seeds=$SEEDS --host=$HOST --port=4445 &
-echo $APP $BEROOT -H $APPHOST -p $APPPORT -n 4445 -w $WORKERPORT
-$APP $BEROOT -H $APPHOST -p $APPPORT -n 4445 -w $WORKERPORT
+echo $APP $BEROOT -H $APPHOST -p $APPPORT -n 4445 -w $WORKERPORT --threads_proportion=2
+$APP $BEROOT -H $APPHOST -p $APPPORT -n 4445 -w $WORKERPORT --threads_proportion=2
