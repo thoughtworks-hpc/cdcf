40d9ec2680d8f703c1b9676df6e4790b0016b1af
Yujia.Li <superxcgm@gmail.com>
2020-05-29 14:26:17 +0800

    Extract variable, remove duplicate

---
+++ node_keeper/src/mock_failure_detector_queue.h
@@ -0,0 +1,15 @@
+//
+// Created by Yujia Li  on 2020/5/29.
+//
+
+#ifndef NODE_KEEPER_SRC_MOCK_FAILURE_DETECTOR_QUEUE_H
+#define NODE_KEEPER_SRC_MOCK_FAILURE_DETECTOR_QUEUE_H
+
+#include "queue.h"
+
+//namespace queue {
+//  class MockFailureDetectorQueue: queue::AbstractQueue {
+//  };
+//}
+
+#endif  // NODE_KEEPER_SRC_MOCK_FAILURE_DETECTOR_QUEUE_H
