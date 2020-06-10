49fb5270176126c7e090e146bc070671f8843fc8
Yujia.Li <superxcgm@gmail.com>
2020-06-10 11:07:19 +0800

    Fix format error & add expect_call

--- daemon/src/daemon_test.cc
+++ daemon/src/daemon_test.cc
@@ -10,14 +10,18 @@
 class MockProcessManager : public ProcessManager {
  public:
   MOCK_METHOD(std::shared_ptr<void>, NewProcessInfo, (), (override));
-  MOCK_METHOD(void, CreateProcess, (const std::string &, const std::vector<std::string>&, std::shared_ptr<void>),
+  MOCK_METHOD(void, CreateProcess,
+              (const std::string &, const std::vector<std::string> &,
+               std::shared_ptr<void>),
               (override));
 };
 
-TEST(Daemon, should_call_process_manager_to_create_pocess) {
+TEST(ADaemon, should_call_process_manager_to_create_pocess) {
   MockProcessManager mock_process_manager;
   const char *path = "/bin/ls";
-  EXPECT_CALL(mock_process_manager, CreateProcess(testing::_, testing::_, testing::_));
+  EXPECT_CALL(mock_process_manager, NewProcessInfo());
+  EXPECT_CALL(mock_process_manager,
+              CreateProcess(testing::_, testing::_, testing::_));
 
   Daemon d(mock_process_manager, path, {"-l"});
 }
