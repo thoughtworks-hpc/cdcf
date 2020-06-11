5f0a7878180c72f90b9cb0ed56603d644ac6ef6c
Yujia.Li <superxcgm@gmail.com>
2020-06-11 18:10:57 +0800

    Fix optional app-args temp

--- docker/script.sh
+++ docker/script.sh
@@ -4,7 +4,8 @@ echo "seeds: $SEEDS"
 NAME=${NAME:-$HOST}
 echo "name: ${NAME}"
 if [ -n "$APP_ARGS" ]; then
-  OPTION_APP_ARGS="--app-args='${APP_ARGS}'"
+  echo /bin/node_keeper --name=$NAME --seeds=$SEEDS --host=$HOST --port=4445 $LOG_COMMANDS --app=$APP --app-args="${APP_ARGS}"
+  /bin/node_keeper --name=$NAME --seeds=$SEEDS --host=$HOST --port=4445 $LOG_COMMANDS --app=$APP --app-args="${APP_ARGS}"
 fi
-echo /bin/node_keeper --name=$NAME --seeds=$SEEDS --host=$HOST --port=4445 $LOG_COMMANDS --app=$APP OPTION_APP_ARGS
-/bin/node_keeper --name=$NAME --seeds=$SEEDS --host=$HOST --port=4445 $LOG_COMMANDS --app=$APP OPTION_APP_ARGS
+echo /bin/node_keeper --name=$NAME --seeds=$SEEDS --host=$HOST --port=4445 $LOG_COMMANDS --app=$APP
+/bin/node_keeper --name=$NAME --seeds=$SEEDS --host=$HOST --port=4445 $LOG_COMMANDS --app=$APP
