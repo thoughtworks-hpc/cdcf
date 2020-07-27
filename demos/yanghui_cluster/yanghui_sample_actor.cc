83fe0758cb461e6477072e0877a55f20663f719d
davidzwb <davidzwb@gmail.com>
2020-07-27 15:33:47 +0800

    Add debug output

--- demos/yanghui_cluster/yanghui_sample_actor.cc
+++ demos/yanghui_cluster/yanghui_sample_actor.cc
@@ -15,11 +15,17 @@ caf::behavior yanghui_get_final_result(caf::stateful_actor<GetMinState>* self,
       [=](const std::vector<int>& data) {
         if (!self->state.message_sender) {
           self->state.message_sender = self->current_sender();
+          std::cout << "yanghui_get_final_result 1: "
+                    << caf::to_string(self->state.message_sender->address())
+                    << std::endl;
         }
         int len = data.size();
         if (1 == len) {
           std::cout << "final result:" << data[0] << std::endl;
           self->send(out_data, data[0]);
+          std::cout << "yanghui_get_final_result 2: "
+                    << caf::to_string(self->state.message_sender->address())
+                    << std::endl;
           self->send(caf::actor_cast<caf::actor>(self->state.message_sender),
                      data[0]);
           std::cout << "final send finish" << std::endl;
@@ -99,7 +105,7 @@ caf::behavior yanghui_count_path(caf::stateful_actor<YanghuiState>* self,
 
         if (index == self->state.data.size()) {
           std::cout << "count path task finish." << std::endl;
-          self->send(out_data, self->state.data[index - 1]);
+          //          self->send(out_data, self->state.data[index - 1]);
           self->send(caf::actor_cast<caf::actor>(self->state.message_sender),
                      self->state.data[index - 1]);
           return;
