7b06545d2fd36a3a886c64227158924a9cea2f43
cx-Zh <491907188@qq.com>
2020-07-28 17:04:53 +0800

    Add result print and change send to request

--- demos/yanghui_cluster/yanghui_example_v2.cc
+++ demos/yanghui_cluster/yanghui_example_v2.cc
@@ -545,20 +545,29 @@ bool SendJobAndCheckResult(caf::actor_system& system, caf::scoped_actor& self,
                            int result_to_check) {
   bool running_status_normal = true;
 
-  self->send(job_actor, yanghui_data);
-  self->receive(
-      [&](bool status, int result) {
-        aout(self) << "status: " << status << " -> " << result << std::endl;
-        if (!status || result != result_to_check) {
-          aout(self) << "inconsistent job: " << job_actor.id() << std::endl;
-          running_status_normal = false;
-        }
-      },
-      [&](caf::error& err) {
-        aout(self) << "failed job: " << job_actor.id() << " : "
-                   << system.render(err) << std::endl;
-        running_status_normal = false;
-      });
+  self->request(job_actor, std::chrono::seconds(180), yanghui_data)
+      .receive(
+          [&](bool status, int result) {
+            if (!status) {
+              aout(self) << "failed job: " << job_actor.id()
+                         << " : calculation error" << std::endl;
+              running_status_normal = false;
+            } else {
+              if (result != result_to_check) {
+                aout(self) << "inconsistent job: " << job_actor.id()
+                           << std::endl;
+                running_status_normal = false;
+              } else {
+                aout(self) << "succeeded job: " << job_actor.id()
+                           << ", with result: " << result << std::endl;
+              }
+            }
+          },
+          [&](caf::error& err) {
+            aout(self) << "failed job: " << job_actor.id() << " : "
+                       << system.render(err) << std::endl;
+            running_status_normal = false;
+          });
 
   return running_status_normal;
 }
@@ -585,6 +594,7 @@ void SillyClientStart(caf::actor_system& system, const config& cfg) {
       std::cout << "sending job to yanghui_job " << i << std::endl;
       running_status_normal = SendJobAndCheckResult(
           system, self, yanghui_jobs[i], yanghui_data, yanghui_job_result);
+      std::this_thread::sleep_for(std::chrono::seconds(2));
     }
   }
 }
