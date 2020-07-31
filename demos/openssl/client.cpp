86cf6e237c72398f38c831ce89bd59379a2c1df2
Yujia.Li <superxcgm@gmail.com>
2020-07-31 11:06:43 +0800

    Fix cpp lint

--- demos/openssl/client.cpp
+++ demos/openssl/client.cpp
@@ -1,54 +1,61 @@
+#include <iostream>
+#include <string>
+
 #include "caf/all.hpp"
 #include "caf/io/all.hpp"
 #include "caf/openssl/all.hpp"
-#include <string>
-#include <iostream>
 
 using namespace caf;
 
-caf::actor connect_tcp_worker(caf::actor_system &system, const std::string &host) {
-    auto result = caf::io::remote_actor(system, host, 50602);
-    if (!result) {
-        std::cout << "connect failed, reason: " << system.render(result.error()) << std::endl;
-        exit(1);
-    }
-    std::cout << "address: " << caf::to_string(result->address()) << std::endl;
-    return *result;
+caf::actor connect_tcp_worker(caf::actor_system &system,
+                              const std::string &host) {
+  auto result = caf::io::remote_actor(system, host, 50602);
+  if (!result) {
+    std::cout << "connect failed, reason: " << system.render(result.error())
+              << std::endl;
+    exit(1);
+  }
+  std::cout << "address: " << caf::to_string(result->address()) << std::endl;
+  return *result;
 }
 
-caf::actor connect_ssl_worker(caf::actor_system &system, const std::string &host) {
-    auto result_ssl = caf::openssl::remote_actor(system, host, 50603);
-    if (!result_ssl) {
-        std::cout << "ssl connect failed, reason: " << system.render(result_ssl.error()) << std::endl;
-        exit(1);
-    }
-    std::cout << "address: " << caf::to_string(result_ssl->address()) << std::endl;
-    return *result_ssl;
+caf::actor connect_ssl_worker(caf::actor_system &system,
+                              const std::string &host) {
+  auto result_ssl = caf::openssl::remote_actor(system, host, 50603);
+  if (!result_ssl) {
+    std::cout << "ssl connect failed, reason: "
+              << system.render(result_ssl.error()) << std::endl;
+    exit(1);
+  }
+  std::cout << "address: " << caf::to_string(result_ssl->address())
+            << std::endl;
+  return *result_ssl;
 }
 
 [[noreturn]] void caf_main(actor_system &system) {
-    std::string dummy;
-//    std::cout << "Please input remote host: ";
-//    std::getline(std::cin, dummy);
-//    std::string host = dummy;
-    std::string host = "localhost";
-    auto spong = connect_tcp_worker(system, host);
-    auto spong_ssl = connect_ssl_worker(system, host);
+  std::string dummy;
+  //    std::cout << "Please input remote host: ";
+  //    std::getline(std::cin, dummy);
+  //    std::string host = dummy;
+  std::string host = "localhost";
+  auto spong = connect_tcp_worker(system, host);
+  auto spong_ssl = connect_ssl_worker(system, host);
 
-    while (true) {
-        std::getline(std::cin, dummy);
-        if (dummy == "n") {
-            caf::anon_send(spong, "hello");
-            continue;
-        }
-        if (dummy == "s") {
-            caf::anon_send(spong_ssl, "hello");
-            continue;
-        }
+  while (true) {
+    std::getline(std::cin, dummy);
+    if (dummy == "n") {
+      caf::anon_send(spong, "hello");
+      continue;
     }
+    if (dummy == "s") {
+      caf::anon_send(spong_ssl, "hello");
+      continue;
+    }
+  }
 }
 
 CAF_MAIN(caf::io::middleman, caf::openssl::manager)
 // Run configuration
-// program args: --openssl.cafile=ca-noenc.crt --openssl.certificate=node2.cert.pem --openssl.key=node2.key.pem
-// working directory: /path/to/docker/ssl
+// program args: --openssl.cafile=ca-noenc.crt
+// --openssl.certificate=node2.cert.pem --openssl.key=node2.key.pem working
+// directory: /path/to/docker/ssl
