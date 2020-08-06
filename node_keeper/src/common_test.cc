72420f73afc405cbc921191fe185544daefeb0d0
Yujia.Li <superxcgm@gmail.com>
2020-08-06 17:53:53 +0800

    Fix split error

---
+++ node_keeper/src/common_test.cc
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2020 ThoughtWorks Inc.
+ */
+
+#include <gmock/gmock.h>
+#include "src/common.h"
+
+TEST(SplitTest, shoud_split_correctly_given_one_space_seprate) {
+  auto got = node_keeper::split("a b c", ' ');
+  std::vector<std::string> want = {"a", "b", "c"};
+
+  ASSERT_THAT(got, testing::Eq(want));
+}
+
+TEST(SplitTest, shoud_split_correctly_given_more_space_seprate) {
+  auto got = node_keeper::split("a b  c   d", ' ');
+  std::vector<std::string> want = {"a", "b", "c", "d"};
+
+  ASSERT_THAT(got, testing::Eq(want));
+}
