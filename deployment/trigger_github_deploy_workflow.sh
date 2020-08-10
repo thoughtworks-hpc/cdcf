b8e50b24e7aa7ef3a3b0e2128fc569f94343cceb
David Zhao <davidzwb@gmail.com>
2020-08-10 18:12:42 +0800

    Add script for manual triggering of deployment

---
+++ deployment/trigger_github_deploy_workflow.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+curl \
+  -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/thoughtworks-hpc/cdcf/actions/workflows/publish_and_deploy_image.yml/dispatches \
+  -d '{"ref":"yanghui_cluster_mod"}'
