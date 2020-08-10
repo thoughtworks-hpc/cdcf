a3d17ddabd58efc36503cd4144a1db05e8e07dba
cx-Zh <491907188@qq.com>
2020-08-10 18:21:57 +0800

    Optimize script to accept user access token

--- deployment/trigger_github_deploy_workflow.sh
+++ deployment/trigger_github_deploy_workflow.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
 curl \
+  -u "${1}" \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/thoughtworks-hpc/cdcf/actions/workflows/publish_and_deploy_image.yml/dispatches \
