95a0bd6d47a07257ad30e0a8ea5243688366f4c6
yuecheng.pei <793499453@qq.com>
2020-08-12 12:46:15 +0800

    Add extra test for If Quit a Actor

--- router_pool/src/router_pool_test_config.h
+++ router_pool/src/router_pool_test_config.h
@@ -14,11 +14,20 @@ simple_calculator::behavior_type simple_calculator_fun(
     simple_calculator::pointer self) {
   self->set_default_handler(caf::reflect_and_quit);
   return {[=](int a, int b) {
-    CDCF_LOGGER_INFO(
-        "actor_id:#{} received add_atom task from remote node. input a:{} b:{}",
-        self->id(), a, b);
-    auto msg = caf::make_message(a + b, self->id());
-    return msg;
+    if (a == -1 && b == -1) {
+      self->quit();
+      CDCF_LOGGER_ERROR(
+          "actor_id:#{} received quit msg from remote node. input a:{} b:{}",
+          self->id(), a, b);
+      return caf::make_message(caf::make_error(caf::sec::bad_function_call));
+
+    } else {
+      CDCF_LOGGER_INFO(
+          "actor_id:#{} received add_atom task from remote node. input a:{} "
+          "b:{}",
+          self->id(), a, b);
+      return caf::make_message(a + b, self->id());
+    }
   }};
 }
 
