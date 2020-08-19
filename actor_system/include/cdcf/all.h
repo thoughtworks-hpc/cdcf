d0aad8f34ac76f18fb7d42a88dffeb68dc0e1319
Yujia.Li <superxcgm@gmail.com>
2020-08-19 09:45:53 +0800

    Use cpack to generate package

---
+++ actor_system/include/cdcf/all.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2020 ThoughtWorks Inc.
+ */
+
+#ifndef CDCF_ALL_H
+#define CDCF_ALL_H
+
+#include <cdcf/cdcf_config.h>
+#include <cdcf/logger.h>
+
+#include "cdcf/actor_guard.h"
+#include "cdcf/actor_monitor.h"
+#include "cdcf/actor_status_monitor.h"
+#include "cdcf/actor_status_service_grpc_impl.h"
+#include "cdcf/actor_union.h"
+#include "cdcf/cluster/cluster.h"
+#include "cdcf/load_balancer/load_balancer.h"
+#include "cdcf/load_balancer/policy.h"
+#include "cdcf/message_priority_actor.h"
+#include "cdcf/router_pool/router_pool.h"
+
+#endif  // CDCF_ALL_H
