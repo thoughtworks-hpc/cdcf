c6734e6dae9fb21ff1db4cf88b3f48097d1c42d6
Yujia.Li <superxcgm@gmail.com>
2020-09-28 14:13:54 +0800

    Fix lint format

--- node_keeper/src/membership.cc
+++ node_keeper/src/membership.cc
@@ -340,7 +340,7 @@ void membership::Membership::HandleGossip(const struct gossip::Address& node,
     RecoverySuspect(member);
   } else if (message.IsActorSystemDownMessage()) {
     CDCF_LOGGER_INFO("Receive gossip actors system down message for {}:{}",
-                      member.GetNodeName(), member.GetPort());
+                     member.GetNodeName(), member.GetPort());
     if (!IfBelongsToMembers(member)) {
       return;
     }
@@ -351,7 +351,7 @@ void membership::Membership::HandleGossip(const struct gossip::Address& node,
     MergeActorSystemDown(member, message.GetIncarnation());
   } else if (message.IsActorSystemUpMessage()) {
     CDCF_LOGGER_INFO("Receive gossip actors system up message for {}:{}",
-                      member.GetNodeName(), member.GetPort());
+                     member.GetNodeName(), member.GetPort());
     if (!IfBelongsToMembers(member)) {
       return;
     }
@@ -833,7 +833,7 @@ void membership::Membership::MergeActorSystemUp(
     const std::lock_guard<std::mutex> lock(mutex_member_actor_system_);
     member_actor_system_[member] = true;
     CDCF_LOGGER_INFO("merge actor system up success. member: {}:{}",
-                      member.GetNodeName(), member.GetPort());
+                     member.GetNodeName(), member.GetPort());
   }
 
   Notify();
@@ -855,7 +855,7 @@ void membership::Membership::MergeActorSystemDown(
     const std::lock_guard<std::mutex> lock(mutex_member_actor_system_);
     member_actor_system_[member] = false;
     CDCF_LOGGER_INFO("merge actor system down success. member: {}:{}",
-                      member.GetNodeName(), member.GetPort());
+                     member.GetNodeName(), member.GetPort());
   }
 
   Notify();
@@ -869,7 +869,7 @@ void membership::Membership::NotifyActorSystemDown() {
   gossip::Payload payload(serialized.data(), serialized.size());
   SendGossip(payload);
   CDCF_LOGGER_INFO("send self actor system down gossip, incarnation: ",
-                    message.GetIncarnation());
+                   message.GetIncarnation());
 }
 std::map<membership::Member, bool> membership::Membership::GetActorSystems()
     const {
@@ -885,7 +885,9 @@ void membership::Membership::SendSelfActorSystemUpGossip() {
   auto serialized = message.SerializeToString();
   gossip::Payload payload(serialized.data(), serialized.size());
   SendGossip(payload);
-  CDCF_LOGGER_INFO("send self actor system up gossip, Member: {}, Incarnation: {}", message.GetMember(), message.GetIncarnation());
+  CDCF_LOGGER_INFO(
+      "send self actor system up gossip, Member: {}, Incarnation: {}",
+      message.GetMember(), message.GetIncarnation());
 }
 
 bool membership::operator==(const membership::Member& lhs,
